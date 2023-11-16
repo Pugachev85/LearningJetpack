486fffb720c1cdda0cd434d71a985329bacfa26a
Pugachev85 <workpost2008@mail.ru>
2023-11-16 22:52:14 +0300

    Lesson_4

--- app/src/main/java/com/pugachev85/learningjetpack/MainActivity.kt
+++ app/src/main/java/com/pugachev85/learningjetpack/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.pugachev85.learningjetpack
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -19,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -27,7 +36,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                ListItem(name = "Maxim Averin", prof = "Actor")
+                ListItem(name = "Maxim Averin", prof = "Actor")
+                ListItem(name = "Maxim Averin", prof = "Actor")
+                ListItem(name = "Maxim Averin", prof = "Actor")
+                ListItem(name = "Maxim Averin", prof = "Actor")
                 ListItem(name = "Maxim Averin", prof = "Actor")
                 ListItem(name = "Maxim Averin", prof = "Actor")
                 ListItem(name = "Maxim Averin", prof = "Actor")
@@ -44,7 +58,16 @@ private fun ListItem(name: String, prof: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(10.dp)
+            .pointerInput(Unit) {
+//                detectDragGesturesAfterLongPress { change, dragAmount ->
+//                    Log.d("MyLog", "LongPress $dragAmount")
+//                }
+                detectTapGestures {
+                    Log.d("MyLog", "Tap offset $it")
+                }
+            }
+        ,
         shape = RoundedCornerShape(15.dp),
         elevation = CardDefaults.cardElevation(5.dp)
     ) {
